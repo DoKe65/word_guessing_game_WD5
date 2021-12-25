9c31e44db502439590896bc82b5466508983df7f
Doris Keller <aurelia65@gmail.com>
2021-12-25 11:51:27 +0100

    Added additional fixes and exceeds requirements

---
+++ README.md
@@ -0,0 +1,5 @@
+# Game Show App
+This is the 6. project for the Treehouse Web Developement Techdegree.
+
+## Goal
+Creating a virtual version of "Wheel of Fortune" with javascript, where the player has to guess random phrases. 
---
+++ app.js
@@ -0,0 +1,122 @@
+// Global Variables
+const startScreen = document.getElementById("overlay");
+const keyboard = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
+const phrasesUl = phrase.firstElementChild;
+const hearts = document.querySelectorAll(".tries");
+const lostHearts = `<img src="images/faded-rose.png" height="60px" width="auto">`;
+let missed = 0;
+
+// Array with phrases to guess
+const phrases = [
+  "Sleep is the mother of great ideas",
+  "I love to code",
+  "Live each day as if it were the last",
+  "Step by step towards the goal",
+  "Learning to code is fun",
+  "Lifelong learners are more content",
+  "Stay on your feet",
+  "Make the best out of what you have",
+  "Be patient with your brain",
+  "Cherish your inner child"
+];
+
+// Chose a random phrase to be displayed from an array with phrases
+function getRandomPhraseAsArray(arr) {
+  const index = Math.floor(Math.random() * arr.length);
+  const phraseToDisplay = arr[index].toUpperCase().split("");
+  return phraseToDisplay;
+}
+
+// Display the randomly chosen array inside the phrases section
+function addPhraseToDisplay(arr) {
+
+  for (let i = 0; i < arr.length; i++) {
+
+    const li = document.createElement("li");
+    li.textContent = arr[i];
+    phrasesUl.appendChild(li);
+
+    if (arr[i] === " ") {
+      li.className = "space";
+    } else {
+      li.className = "letter";
+    }
+  }
+}
+
+// Check if a letter pressed inside the keyboard display is present in the phrase to guess
+function checkLetter(charBtn) {
+  let phraseToCheck = phrasesUl.getElementsByClassName("letter");
+  let char = null;
+  for (let i = 0; i < phraseToCheck.length; i++) {
+    if (phraseToCheck[i].textContent === charBtn.textContent.toUpperCase()) {
+      phraseToCheck[i].classList.add("show");
+      charBtn.setAttribute("disabled", true);
+      char = charBtn.textContent;
+    }
+  }
+  return char;
+}
+
+function clearGame () {
+  // reset missed lives
+  missed = 0;
+  // delete the previously displayed phrase
+  phrasesUl.innerHTML = "";
+  // remove the attribute "disabled" and the class "chosen" from used keys
+  const quertyKeys = keyboard.querySelectorAll(".chosen");
+  for (let i = 0; i < quertyKeys.length; i++) {
+    quertyKeys[i].removeAttribute("disabled");
+    quertyKeys[i].classList.remove("chosen");
+  }
+  // replace lost hearts with live hearts
+  const ol = document.querySelector("ol");
+  const lives = ol.querySelectorAll("img");
+  for (let i = 0; i < lives.length; i++) {
+    lives[i].setAttribute('src', 'images/rose.png');
+  }
+}
+
+function checkWin () {
+  const foundLettersLength = phrasesUl.querySelectorAll(".show").length;
+  const phraseLength = phrasesUl.querySelectorAll(".letter").length;
+
+  if(foundLettersLength === phraseLength) {
+    clearGame();
+    startScreen.className = "win";
+    startScreen.style.display = "flex";
+  } else if (missed >= 5) {
+    clearGame();
+    startScreen.className = "lose";
+    startScreen.style.display = "flex";
+  }
+}
+
+// Keyboard event listener - should only apply to buttons pressed inside the keyboard display
+keyboard.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.tagName === "BUTTON") {
+    target.setAttribute("disabled", "true");
+    target.className = "chosen";
+    if(!target.getAttribute.disabled) {
+      const letterFound = checkLetter(target);
+      if (letterFound === null) {
+        missed++;
+        hearts[missed-1].innerHTML = lostHearts;
+      }
+    }
+  }
+  checkWin();
+});
+
+// Hide the start overlay when clicking the Start button
+startScreen.addEventListener("click", (event) => {
+  if(event.target.className === "btn__reset") {
+    startScreen.style.display = "none";
+    document.querySelector(".header").style.backgroundColor = "#4e5e3aa6";
+    // clear what was before
+    const phraseToDisplay = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseToDisplay);
+  }
+});
